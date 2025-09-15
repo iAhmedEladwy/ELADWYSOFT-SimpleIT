82c6f16ae77fe376322cfd3c0574d25f33bef3b6
Ahmed Eladwy <eladwy.ahmed@gmail.com>
2025-09-15 14:53:28 +0300

    Adjust filters width

--- client/src/pages/AssetHistory.tsx
+++ client/src/pages/AssetHistory.tsx
@@ -529,7 +529,7 @@ export default function AssetHistory() {
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-4">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-2">
               {/* Search */}
               <div className="space-y-2">
                 <Label htmlFor="search">{translations.search}</Label>
